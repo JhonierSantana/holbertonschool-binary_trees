136ccc73a74465a25cb9eab0de286acd0c576002
root <root@DESKTOP-ISQ43BP>
2023-01-17 11:23:33 -0500

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,9 +11,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	if (tree->left || tree)
+	if (tree->left)
 		left_height = 1 + binary_tree_height(tree->left);
-	if (tree->right || tree)
+	if (tree->right)
 		right_height = 1 + binary_tree_height(tree->right);
 	return (right_height > left_height ? right_height : left_height);
 }
