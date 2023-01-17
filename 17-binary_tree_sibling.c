46bb9457ac94e1f14853acc5b42a897fd3ba822b
Natha0b <laura.barona04@gmail.com>
2023-01-17 18:05:53 -0500

    create the files and add code

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,9 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: pointer to the root node of the tree to measure the height.
+ * Return: zero or one.
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+}
