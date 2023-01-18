dcf8df61c3025e11aa82ae753187856bc5f716a6
root <root@DESKTOP-ISQ43BP>
2023-01-18 15:30:41 -0500

    17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,9 +1,16 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_is_full - checks if a binary tree is full
- * @tree: pointer to the root node of the tree to measure the height.
- * Return: zero or one.
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: pointer to the node to find the sibling
+ * Return: Pointer to sibling, or NULL if no sibling is found
  */
+
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+	if (!node || !(node->parent))
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
 }
